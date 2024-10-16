1e1c5b48615487d717f69c67135b636bc4d0fb6f
DastenA <150040523+DastenA@users.noreply.github.com>
2024-10-16 11:35:13 +0200

    AHHH

--- app.rb
+++ app.rb
@@ -1,1 +1,15 @@
-print 'arne'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'slim'
+
+get ('/one/:number') do
+    @data = params[:number].to_i
+    slim :one
+end
+
+
+get ('/two') do
+    @lista = ["gurka", "majs", "sallad", "paprika"]
+    slim :two
+
+end
