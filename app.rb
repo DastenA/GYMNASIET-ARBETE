0875c0db628f999ed61f72bd75a36004de74a16c
ArnePalmbergNTI2C <150041090+ArnePalmbergNTI2C@users.noreply.github.com>
2024-10-22 15:59:26 +0200

    okej

--- app.rb
+++ app.rb
@@ -2,10 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
 
-get ('/one') do
-    slim :one
+post ('/one_p') do
+    @data_one = params[:secret_one]
+    p @data_one
+    redirect('/one')
+
 end
 
+get ('/one') do
+
+    slim :one
+
+end
+
+post ('/two_p') do
+    @data_two = params[:secret_two]
+    p @data_two
+    redirect('/two')
+end
 
 get ('/two') do
     slim :two
