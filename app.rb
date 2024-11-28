ce8aae1e10934ac13d6bc8260c2287b793430e5b
ArnePalmbergNTI2C <150041090+ArnePalmbergNTI2C@users.noreply.github.com>
2024-11-28 23:01:48 +0100

    omg

--- app.rb
+++ app.rb
@@ -1,7 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
-require 'minimagick'
+require 'rubygems'
+require 'rmagick'
+
+require 'rmagick'
+
+# Ladda bilden
+image = Magick::Image.read('vit.jpg').first
+
+# Gå igenom varje pixel och ändra dess värden
+image.each_pixel do |pixel, x, y|
+  # Ändra värden och klipp till intervallet [0, 255]
+  red   = [[pixel.red / 256 - 100, 0].max, 255].min
+  green = [[pixel.green / 256 - 100, 0].max, 255].min
+  blue  = [[pixel.blue / 256 - 100, 0].max, 255].min
+
+  # Uppdatera pixeln
+  image.pixel_color(x, y, Magick::Pixel.new(red * 256, green * 256, blue * 256))
+end
+
+# Spara den ändrade bilden
+image.write('output_image.jpg')
+puts "Bilden har sparats som output_image.jpg"
+
 enable :sessions
 
 #kryptera
